b5f060ea4b1c1d8e9c4ef4a45c1b2ae385e0fae8
sorawot(kem) <sorawit.kam@gmail.com>
2025-09-23 09:57:19 +0700

    Update

---
+++ SearchSP.sql
@@ -0,0 +1,16 @@
+SELECT 
+    o.name AS [ProcedureName]
+    ,o.schema_id
+    ,s.name AS [SchemaName]   
+FROM 
+    sys.sql_modules m
+INNER JOIN 
+    sys.objects o ON m.object_id = o.object_id
+INNER JOIN 
+    sys.schemas s ON o.schema_id = s.schema_id
+WHERE 
+    m.definition LIKE '%usp_BillingRequest_Sub01_Insert_V2%'
+ --AND m.definition LIKE '@ApproveStatusId'
+    AND o.type = 'P'  -- 'P' stands for stored procedures
+ORDER BY 
+    s.name, o.name;
