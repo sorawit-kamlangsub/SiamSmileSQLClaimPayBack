2f5cb97ce405254658801e16b8c71fb415875d67
sorawot(kem) <sorawit.kam@gmail.com>
2025-10-10 10:20:27 +0700

    upsert

---
+++ SiamSmileSQLClaimPayBack/usp_BillingRequest_Insert.sql
@@ -0,0 +1,31 @@
+﻿USE [ClaimPayBack]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_BillingRequest_Insert]    Script Date: 10/10/2568 10:19:40 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Napaporn Saarnwong
+-- Create date: 2022-10-31  09:05
+--	Updated date: 2023-02-08 Add input data (@BillingDateTo)
+-- Description:	
+-- =============================================
+ALTER PROCEDURE [dbo].[usp_BillingRequest_Insert]
+	@CreatedByUserId	INT				--CreatedByUserId and UpdatedByUserId
+	,@BillingDateTo		DATE 
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+--EXECUTE  [dbo].[usp_BillingRequest_Insert_V1] @CreatedByUserId;
+
+
+EXECUTE  [dbo].[usp_BillingRequest_Insert_V3] @CreatedByUserId, @BillingDateTo;
+
+
+
+END;
+
