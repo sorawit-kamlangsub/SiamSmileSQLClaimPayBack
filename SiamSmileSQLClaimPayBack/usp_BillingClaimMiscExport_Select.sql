7745578dcb4e6de0407db1748f43c2e11f8bd1a7
sorawot(kem) <sorawit.kam@gmail.com>
2025-11-03 11:06:46 +0700

    update

--- SiamSmileSQLClaimPayBack/usp_BillingClaimMiscExport_Select.sql
+++ SiamSmileSQLClaimPayBack/usp_BillingClaimMiscExport_Select.sql
@@ -1,6 +1,6 @@
 USE [ClaimPayBack]
 GO
-/****** Object:  StoredProcedure [Claim].[usp_BillingClaimMiscExport_Select]    Script Date: 31/10/2568 16:31:43 ******/
+/****** Object:  StoredProcedure [dbo].[usp_BillingClaimMiscExport_Select]    Script Date: 31/10/2568 16:31:43 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -12,7 +12,7 @@ GO
 -- Create date: 2025-10-31 16:30
 -- Description:	For Export Claimmisc Billing
 -- =============================================
-ALTER PROCEDURE [Claim].[usp_BillingClaimMiscExport_Select]
+ALTER PROCEDURE [dbo].[usp_BillingClaimMiscExport_Select]
 	 @ClaimCode VARCHAR(20)
 	 ,@BillingRequestGroupCode INT
 AS
