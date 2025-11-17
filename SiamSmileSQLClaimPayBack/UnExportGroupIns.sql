56a2e6025dde989dda75eb575196fcff5366b299
kem <kem@DESKTOP-HSSI3UJ>
2025-11-17 13:36:47 +0700

    add

---
+++ SiamSmileSQLClaimPayBack/UnExportGroupIns.sql
@@ -0,0 +1,33 @@
+USE [ClaimPayBack]
+GO
+
+DECLARE @BillingRequestGroupCode NVARCHAR(50) = 'BQGPH05H6811033'
+,@NewBillingDate			DATE			= '2025-11-17'
+,@CreatedByUserId			INT				= 6772
+
+DECLARE @D2						DATETIME2		= SYSDATETIME();
+DECLARE @Date					DATE = @D2;
+DECLARE @UserId					INT				= @CreatedByUserId;
+
+--SELECT *
+UPDATE	m 
+	SET m.ClaimHeaderGroupImportStatusId	= 2
+	,BillingRequestGroupId					= NULL
+	,m.UpdatedDate							= @D2
+	,m.UpdatedByUserId						= @UserId
+	,m.BillingDate							= @NewBillingDate
+FROM dbo.ClaimHeaderGroupImport m
+	INNER JOIN dbo.BillingRequestGroup bg
+		ON bg.BillingRequestGroupId = m.BillingRequestGroupId	
+WHERE bg.BillingRequestGroupCode = @BillingRequestGroupCode
+--WHERE m.CreatedDate >= @NewBillingDate
+
+--SELECT *
+DELETE be
+FROM dbo.BillingExport AS be
+WHERE be.BillingRequestGroupCode = @BillingRequestGroupCode;
+
+--SELECT *
+DELETE bg
+FROM dbo.BillingRequestGroup AS bg
+WHERE bg.BillingRequestGroupCode = @BillingRequestGroupCode;
