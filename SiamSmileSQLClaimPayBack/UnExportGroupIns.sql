370fe0818cb1bb74010af69ebf02fc6d96d4494f
sorawot(kem) <sorawit.kam@gmail.com>
2025-11-19 09:46:27 +0700

    fix

--- SiamSmileSQLClaimPayBack/UnExportGroupIns.sql
+++ SiamSmileSQLClaimPayBack/UnExportGroupIns.sql
@@ -1,32 +1,59 @@
 USE [ClaimPayBack]
 GO
 
-DECLARE @BillingRequestGroupCode NVARCHAR(50) = 'BQGPH05H6811009'
+DECLARE @BillingRequestGroupCode NVARCHAR(50) = 'BQGPH39H6811010'
 ,@NewBillingDate			DATE			= '2025-11-19'
-,@CreatedByUserId			INT				= 6772
+,@UpdatedByUserId			INT				= 6772
 
-DECLARE @D2						DATETIME2		= SYSDATETIME();
-DECLARE @Date					DATE = @D2;
-DECLARE @UserId					INT				= @CreatedByUserId;
+DECLARE @D2							DATETIME2	= SYSDATETIME();
+DECLARE @Date						DATE		= @D2;
+DECLARE @UserId						INT			= @UpdatedByUserId;
+DECLARE @BillingRequestGroupId		INT			= 0;
+DECLARE @ClaimHeaderGroupImportId	INT			= 0;
 
---SELECT *
-UPDATE	m 
-	SET m.ClaimHeaderGroupImportStatusId	= 2
-	,BillingRequestGroupId					= NULL
-	,m.UpdatedDate							= @D2
-	,m.UpdatedByUserId						= @UserId
-	,m.BillingDate							= @NewBillingDate
+SELECT *
+--UPDATE	m 
+--	SET m.ClaimHeaderGroupImportStatusId	= 2
+--	,BillingRequestGroupId					= NULL
+--	,m.UpdatedDate							= @D2
+--	,m.UpdatedByUserId						= @UserId
+--	,m.BillingDate							= NULL
 FROM dbo.ClaimHeaderGroupImport m
 	INNER JOIN dbo.BillingRequestGroup bg
 		ON bg.BillingRequestGroupId = m.BillingRequestGroupId	
 WHERE bg.BillingRequestGroupCode = @BillingRequestGroupCode
 
---SELECT *
-DELETE be
+SELECT @BillingRequestGroupId = bg.BillingRequestGroupId
+FROM dbo.BillingRequestGroup AS bg
+WHERE bg.BillingRequestGroupCode = @BillingRequestGroupCode;
+
+SELECT @ClaimHeaderGroupImportId = h.ClaimHeaderGroupImportId
+FROM dbo.ClaimHeaderGroupImport h
+	INNER JOIN dbo.BillingRequestGroup bg
+		ON bg.BillingRequestGroupId = h.BillingRequestGroupId	
+WHERE bg.BillingRequestGroupCode = @BillingRequestGroupCode
+
+SELECT *
+--DELETE be
 FROM dbo.BillingExport AS be
 WHERE be.BillingRequestGroupCode = @BillingRequestGroupCode;
 
---SELECT *
-DELETE bg
+SELECT *
+--DELETE bg
 FROM dbo.BillingRequestGroup AS bg
 WHERE bg.BillingRequestGroupCode = @BillingRequestGroupCode;
+
+SELECT *
+--DELETE bi
+FROM dbo.BillingRequestItem AS bi
+WHERE bi.BillingRequestGroupId = @BillingRequestGroupId;
+
+SELECT *
+--DELETE cover
+FROM dbo.BillingRequestGroupXResultDetail AS cover
+WHERE cover.BillingRequestGroupId = @BillingRequestGroupId;
+
+SELECT *
+--DELETE gc
+FROM dbo.ClaimHeaderGroupImportCancel AS gc
+WHERE gc.ClaimHeaderGroupImportId = @ClaimHeaderGroupImportId;
